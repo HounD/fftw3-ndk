cf3452aca3b272fd429395d0579e79c0c86b266d
Vladislav Shikhov <vladshikhov@gmail.com>
2015-01-26 16:18:54 +0300

    module libusb added

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,4 +1,4 @@
 
-
 TOP_DIR := $(call my-dir)
 include $(TOP_DIR)/fftw3/lib/jni/Android.mk
+include $(TOP_DIR)/usb/lib/android/jni/Android.mk
---
+++ jni/usb/Application.mk
@@ -0,0 +1,5 @@
+
+APP_PROJECT_PATH := $(call my-dir)/lib/android
+APP_MODULES      += usb
+APP_OPTIM        := release
+APP_ABI          := armeabi armeabi-v7a
